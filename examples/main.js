8ebcb9936f6d9a992596c7624473f6571430a41f
Kaspars Bulins <kasparsb@hotmail.com>
2015-08-11 18:53:31 +0300

    New config params

--- examples/main.js
+++ examples/main.js
@@ -1,90 +1,107 @@
 (function(){
 
-	var swipe;
-	var el = $(".swipe").get(0);
-	var $log = $(".log");
+    var swipe;
+    var el = $(".swipe").get(0);
+    var $log = $(".log");
 
-	function settings() {
-		var direction = $('[name=dir]').val();
-		var minWidth = $('[name=min-width]').val();
-		var minHeight = $('[name=min-height]').val();
-		var minDuration = $('[name=min-duration]').val();
-		
-		var s = {};
+    function settings() {
+        var direction = $('[name=dir]').val();
 
-		if (direction) {
-			s.direction = direction;
-		}
+        var minWidth = $('[name=min-width]').val();
+        var minHeight = $('[name=min-height]').val();
+        var minDuration = $('[name=min-duration]').val();
 
-		if (minWidth) {
-			s.minWidth = minWidth;
-		}
+        var maxWidth = $('[name=max-width]').val();
+        var maxHeight = $('[name=max-height]').val();
+        var maxDuration = $('[name=max-duration]').val();
+        
+        var s = {};
 
-		if (minHeight) {
-			s.minHeight = minHeight;
-		}
+        if (direction) {
+            s.direction = direction;
+        }
 
-		if (minDuration) {
-			s.minDuration = minDuration;
-		}
+        if (minWidth) {
+            s.minWidth = minWidth;
+        }
 
-		return s;
-	}
+        if (minHeight) {
+            s.minHeight = minHeight;
+        }
 
-	function configureSwipe() {
-		if (!swipe) {
-			swipe = new webit.swipe(el, settings())
-				.on("start", function(t){
-					clearLog();
-					logEvent('start', t);
-				})
-				.on("end", function(t){
-					logEvent('end', t);
-				})
-				.on("move", function(t){
-					logEvent('move', t);
-				});
-		}
-		// If swipe initialized, then reconfigure it
-		else {
-			swipe.config(settings());
-		}
-	}
+        if (minDuration) {
+            s.minDuration = minDuration;
+        }
 
-	function clearLog() {
-		$log.find('div').html('');
-	}
+        if (maxWidth) {
+            s.maxWidth = maxWidth;
+        }
 
-	function logEvent(t, ev) {
-		if (t == 'end') {
-			$log.find('.move').html('');
-		}
-		$log.find('.'+t).html(t+objHTML(ev));
-	}
+        if (maxHeight) {
+            s.maxHeight = maxHeight;
+        }
 
-	function objHTML(obj) {
-		var h = '<div class="obj"><div>{</div>';
-		
-		for (var p in obj) {
-			if (typeof obj[p] == 'object') {
-				h += '<div class="p">'+p+': '+objHTML(obj[p])+'</div>';	
-			}
-			else {
-				h += '<div class="p">'+p+': '+obj[p]+'</div>';		
-			}
-		}
-		
-		h += '<div>}</div></div>';
+        if (maxDuration) {
+            s.maxDuration = maxDuration;
+        }
 
-		return h;
-	}
+        return s;
+    }
 
-	$('form').on('submit', function(ev){
-		ev.preventDefault();
+    function configureSwipe() {
+        if (!swipe) {
+            swipe = new webit.swipe(el, settings())
+                .on("start", function(t){
+                    clearLog();
+                    logEvent('start', t);
+                })
+                .on("end", function(t){
+                    logEvent('end', t);
+                })
+                .on("move", function(t){
+                    logEvent('move', t);
+                });
+        }
+        // If swipe initialized, then reconfigure it
+        else {
+            swipe.config(settings());
+        }
+    }
 
-		configureSwipe();
-	});
+    function clearLog() {
+        $log.find('div').html('');
+    }
 
-	configureSwipe();
+    function logEvent(t, ev) {
+        if (t == 'end') {
+            $log.find('.move').html('');
+        }
+        $log.find('.'+t).html(t+objHTML(ev));
+    }
+
+    function objHTML(obj) {
+        var h = '<div class="obj"><div>{</div>';
+        
+        for (var p in obj) {
+            if (typeof obj[p] == 'object') {
+                h += '<div class="p">'+p+': '+objHTML(obj[p])+'</div>'; 
+            }
+            else {
+                h += '<div class="p">'+p+': '+obj[p]+'</div>';      
+            }
+        }
+        
+        h += '<div>}</div></div>';
+
+        return h;
+    }
+
+    $('form').on('submit', function(ev){
+        ev.preventDefault();
+
+        configureSwipe();
+    });
+
+    configureSwipe();
 
 })();
